a4513be45ab74d2e85ea86f70d586a1c8f46d575
bendevman <bendevman@gmail.com>
2022-10-10 11:33:39 +0300

    add slider

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,5 +3,6 @@ import '../../fonts/fonts.scss';
 
 import '../../components/btn/btn';
 import '../../components/info-box/info-box';
+import '../../components/slider/slider';
 
 import './home.scss';
